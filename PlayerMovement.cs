71d28ad887eaba8c7b88f9e82f08c58f7ad81c4a
TheOnlyFreshPrince <37582649+TheOnlyFreshPrince@users.noreply.github.com>
2018-04-09 04:54:56 -0700

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+	// Named FixedUpdate because I am using it to mess around with the object's physics
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Add a forward force
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
